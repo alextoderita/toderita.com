d2789bb341fdfd3837321d8055168b5f24a49902
Alex Toderita <alex@alex-ThinkPad-X220.mynet>
2018-01-10 20:34:23 +0000

    pipeline files

---
+++ pipeline/config/Jenkinsfile.groovy
@@ -0,0 +1,35 @@
+node(){
+
+    String jdkTool = tool name: 'JDK8', type: 'hudson.model.JDK'
+    String repo = 'https://github.com/alextoderita/toderita.com.git'
+
+    checkout(repo)
+    createDockerImage("my_tomcat")
+    runDockerContainer("tomcat")
+}
+
+private void checkout(String repo){
+    stage('checkout'){
+        println "Checking out code"
+        checkout([
+            $class: 'GitSCM',
+            branches: [[name: '*/master']],
+            userRemoteConfigs: [[
+                credentialsId: 'toderita-com-git',
+                url: repo
+            ]]
+        ])
+    }
+}
+
+private void createDockerImage( String imageName ){
+	stage('Create Docker image'){
+		sh ' cd infra/docker && sudo docker build -t my_tomcat .'
+	}
+}
+
+private void runDockerContainer(String containerName){
+	stage('Run Docker container'){
+		sh 'sudo docker run -p 9999:8080 -d --name tomcat my_tomcat'
+	}
+}
